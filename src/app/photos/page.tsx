4c46b90e342a39de00552d2116523b5c20fdabc9
iboughtbed <testpythontosanzhar@gmail.com>
2024-09-10 15:06:44 +0500

    feat(photos): add photos page

--- src/app/photos/page.tsx
+++ src/app/photos/page.tsx
@@ -1,3 +1,28 @@
+import Image from "next/image";
+
 export default function PhotosPage() {
-  return <div></div>;
+  const gallery = 9;
+
+  return (
+    <main className="relative mx-auto flex min-h-screen flex-col p-2">
+      <ul
+        id="gallery"
+        className="grid h-full w-full grid-cols-1 gap-2 md:grid-cols-2 xl:grid-cols-3"
+      >
+        {Array.from({ length: gallery }).map((_, index) => (
+          <li
+            key={index}
+            className="relative h-[700px] w-full max-sm:h-[480px]"
+          >
+            <Image
+              alt="Photo"
+              src={`/images/photo-${index + 1}.webp`}
+              className="pointer-events-none absolute inset-0 h-full w-full select-none object-cover"
+              fill
+            />
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
 }

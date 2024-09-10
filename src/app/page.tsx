cfa0813f4804e264998640dbb1d2179ff0eeaaa5
iboughtbed <testpythontosanzhar@gmail.com>
2024-09-10 10:02:12 +0500

    feat(landing): add landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,41 @@
+import { AnimatedName } from "@/components/animated-name";
+import Link from "next/link";
+
 export default function Home() {
-  return <div className="min-h-screen w-full"></div>;
+  return (
+    <main className="relative mx-auto flex min-h-screen max-w-3xl flex-col px-6 py-16">
+      <AnimatedName />
+
+      <div className="space-y-4 leading-snug">
+        <p>
+          I&apos;m a full-stack software engineer and a designer. I finished{" "}
+          <Link
+            href="https://incubator.nfactorial.school"
+            rel="noopener noreferrer"
+            target="_blank"
+            className="underline underline-offset-2"
+          >
+            nFactorial Incubator 2024
+          </Link>
+          .
+        </p>
+        <p>
+          I use Next.js and React to build{" "}
+          <Link href="/projects" className="underline underline-offset-2">
+            open-source projects
+          </Link>
+          . I write{" "}
+          <Link href="/blog" className="underline underline-offset-2">
+            blogs
+          </Link>{" "}
+          about variety of themes, including coding, philosophy and I also do
+          professional shit-posting
+        </p>
+        <p>
+          I&apos;m inspired to learn new things and{" "}
+          <span className="">create and design</span> products.
+        </p>
+      </div>
+    </main>
+  );
 }
---
+++ src/components/animated-name.tsx
@@ -0,0 +1,31 @@
+export function AnimatedName() {
+  return (
+    <h1 className="transition-element mb-6 pt-12 font-medium">
+      <span className="sr-only">Sanzhar Zhangaliyev</span>
+      <span aria-hidden="true" className="group relative block overflow-hidden">
+        <span className="inline-block transition-all duration-300 ease-in-out group-hover:-translate-y-full">
+          {"Sanzhar Zhangaliyev".split("").map((letter, index) => (
+            <span
+              key={index}
+              className="inline-block"
+              style={{ transitionDelay: `${index * 25}ms` }}
+            >
+              {letter === " " ? "\u00A0" : letter}
+            </span>
+          ))}
+        </span>
+        <span className="absolute left-0 top-0 inline-block translate-y-full transition-all duration-300 ease-in-out group-hover:translate-y-0">
+          {"iboughtbed".split("").map((letter, index) => (
+            <span
+              key={index}
+              className="inline-block"
+              style={{ transitionDelay: `${index * 25}ms` }}
+            >
+              {letter}
+            </span>
+          ))}
+        </span>
+      </span>
+    </h1>
+  );
+}

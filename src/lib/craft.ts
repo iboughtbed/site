954f90b5ef1829b2e41ea754e1f823cbf031bfb2
iboughtbed <testpythontosanzhar@gmail.com>
2024-12-09 02:14:52 +0500

    fix: typo on getCraftPostss

--- src/lib/craft.ts
+++ src/lib/craft.ts
@@ -3,5 +3,5 @@ import path from "path";
 import { getMDXData } from "@/lib/mdx";
 
 export function getCraftPosts() {
-  return getMDXData(path.join(process.cwd(), "src", "content", "crafts"));
+  return getMDXData(path.join(process.cwd(), "src", "content", "craft"));
 }

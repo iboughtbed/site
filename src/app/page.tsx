750f97d9abc48f4d69c72b06f36f5e602a984bce
iboughtbed <testpythontosanzhar@gmail.com>
2025-04-03 22:36:49 +0500

    feat: rebranding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,62 +1,49 @@
 import Link from "next/link";
 
-import { AnimatedName } from "@/components/animated-name";
-
 export default function Home() {
+  const projects = [
+    {
+      name: "Parallax Podcast",
+      description:
+        "Parallax is an open-source AI platform solution that gives users the power to generate their own podcasts, audiobooks, and audio articles from prompts, documents, images and web search.",
+      href: "https://prllxhq.com",
+      dates: "2024 - Present",
+    },
+  ];
+
   return (
     <main className="relative mx-auto flex min-h-screen max-w-3xl flex-col px-6 py-16">
-      <AnimatedName />
+      <h1 className="mb-6 pt-12 font-medium">Sanzhar Zhangaliyev</h1>
 
       <div className="mb-6 space-y-4 leading-snug">
         <p>
-          I&apos;m a full-stack software engineer and a designer. I finished{" "}
+          I&apos;m a full-stack software engineer and a designer from
+          Kazakhstan. I&apos;m an alumni of nFactorial Incubator 2024 (Largest
+          Startup Incubator in Central Asia) at 16 years old. I use Next.js and
+          React to build open-source projects.
+        </p>
+        <p>
+          I&apos;m inspired to learn new things, and to create and design
+          products. I have contributed to YC & San&nbsp;Franciso startups at 16
+          years old. I&apos;m currently studying at Nazarbayev Intellectual
+          School in grade 11.
+        </p>
+        <p>
           <Link
-            href="https://incubator.nfactorial.school"
+            href="https://x.com/iboughtbed"
             rel="noopener noreferrer"
             target="_blank"
             className="underline underline-offset-2"
           >
-            nFactorial Incubator 2024
-          </Link>
-          .
-        </p>
-        <p>
-          I use Next.js and React to build{" "}
-          <Link href="/projects" className="underline underline-offset-2">
-            open-source projects
-          </Link>
-          . I write{" "}
-          <Link href="/blog" className="underline underline-offset-2">
-            blogs
+            [X/Twitter]
           </Link>{" "}
-          about variety of themes, including coding, philosophy and I also do
-          professional shit-posting
-        </p>
-        <p>
-          I&apos;m inspired to learn new things, and to{" "}
-          <Link href="/craft" className="underline underline-offset-2">
-            create and design
-          </Link>{" "}
-          products.
-        </p>
-        <p>
-          I&apos;m passionate about open-source and maintain{" "}
           <Link
-            href="https://lab.sanzhar.xyz"
+            href="https://github.com/iboughtbed"
+            rel="noopener noreferrer"
             target="_blank"
             className="underline underline-offset-2"
           >
-            lab.sanzhar.xyz
-          </Link>{" "}
-          — a curated collection of high-quality boilerplates and examples for
-          Next.js applications integrated with popular tools.
-        </p>
-        <p>
-          <Link
-            href="/Sanzhar-Zhangaliyev-CV.pdf"
-            className="underline underline-offset-2"
-          >
-            [Resume]
+            [Github]
           </Link>{" "}
           <Link
             href="https://linkedin.com/in/sanzhar-zhangaliyev"
@@ -67,6 +54,22 @@ export default function Home() {
             [Linkedin]
           </Link>
         </p>
+      </div>
+
+      {/* <hr className="border-none mb-6" /> */}
+
+      <div className="mb-6 space-y-4 leading-snug">
+        {projects.map((p, idx) => (
+          <div key={idx} className="flex flex-col gap-3">
+            <div className="flex justify-between items-center">
+              <a href={p.href} target="_blank" rel="noopener noreferrer">
+                {p.name}
+              </a>
+              <span className="text-sm font-mono">{p.dates}</span>
+            </div>
+            <p>{p.description}</p>
+          </div>
+        ))}
       </div>
     </main>
   );
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,15 +1,8 @@
-import { getBlogPosts } from "@/lib/blog";
-
 export default async function sitemap() {
-  const posts = getBlogPosts().map((post) => ({
-    url: `https://sanzhar.xyz/blog/${post.slug}`,
-    lastModified: new Date().toISOString(),
-  }));
-
-  const routes = ["", "/craft", "/projects"].map((route) => ({
+  const routes = [""].map((route) => ({
     url: `https://sanzhar.xyz${route}`,
     lastModified: new Date().toISOString(),
   }));
 
-  return [...routes, ...posts];
+  return [...routes];
 }
